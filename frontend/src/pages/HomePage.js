421b897305376cef976f9f16fe36d2f90ec4e400
Yemo <113440465+yemoalakija@users.noreply.github.com>
2024-02-27 03:06:45 +0000

    Add key prop to mapped elements in HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -8,7 +8,7 @@ function HomePage() {
       <h1>Latest Phones</h1>
       <Row>
         {products.map(products => (
-            <Col sm={12} md={6} lg={4} xl={3}>
+            <Col key={products._id} sm={12} md={6} lg={4} xl={3}>
                 <h3>{products.name}</h3>
             </Col>
             ))}

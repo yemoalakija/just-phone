b705952ee8bea3677212a7ea8e3450a6ce4a0794
yemoalakija <adeyemo.alakija@gmail.com>
2024-03-07 19:55:54 +0100

    Refactor URL patterns in justphone_api/urls.py

--- justphone_api/urls.py
+++ justphone_api/urls.py
@@ -13,21 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_view
+
+# from .views import root_view
 
 # from django.conf import settings
 # from django.conf.urls.static import static
 # from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", root_view, name="root"),
+    path("admin/", admin.site.urls),
+    # path("", root_view, name="root"),
     # path('', TemplateView.as_view(template_name='index.html')),
-    path('api/products/', include('base.urls.product_urls')),
-    path('api/users/', include('base.urls.user_urls')),
-    path('api/orders/', include('base.urls.order_urls')),
+    path("api/products/", include("base.urls.product_urls")),
+    path("api/users/", include("base.urls.user_urls")),
+    path("api/orders/", include("base.urls.order_urls")),
 ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

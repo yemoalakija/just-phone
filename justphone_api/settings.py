8f468ff16a3f4142d3d64b189d864543468c2f27
yemoalakija <adeyemo.alakija@gmail.com>
2024-03-08 20:34:15 +0100

    reposition static

--- justphone_api/settings.py
+++ justphone_api/settings.py
@@ -200,7 +200,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [BASE_DIR / "frontend/build/static"]
 
 CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
